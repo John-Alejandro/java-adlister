d8bb289eeaadbb52a75d0087346b2a0ccd81faa5
john alejandro <john.e.alejandro01@gmail.com>
2021-12-02 16:12:22 -0600

    Completed form

---
+++ src/main/java/PickColorServlet.java
@@ -0,0 +1,16 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("pick-color")
+public class PickColorServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("pick-color.jsp").forward(r);
+    }
+
+    ;
+}

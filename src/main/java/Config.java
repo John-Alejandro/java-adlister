0218b9a754aecb0c368e20e5b3057713bcc737a6
john alejandro <john.e.alejandro01@gmail.com>
2021-12-07 12:06:55 -0600

    Restore files

---
+++ src/main/java/Config.java
@@ -0,0 +1,11 @@
+public class Config {
+    public String getUrl() {
+        return "jdbc:mysql://localhost:3306/adlister_db?allowPublicKeyRetrieval=true&useSSL=false";
+    }
+    public String getUser() {
+        return "annie";
+    }
+    public String getPassword() {
+        return "thompson331";
+    }
+}
